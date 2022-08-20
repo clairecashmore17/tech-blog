cb5dd09d0e582de14c7e24e018a707e2e6c4e16f
Claire Cashmore <clairecashmore2017@gmail.com>
2022-08-20 00:01:27 -0400

    created helpers and partials

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const sequelize = require("./config/connection");
+
 const path = require("path");
+const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const routes = require("./controllers");
 
 const session = require("express-session");
